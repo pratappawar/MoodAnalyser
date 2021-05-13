b096cef18685a1933cf66d5baf971e0b0c2a05b5
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-13 06:56:36 +0530

    Added: Test cases using Exception

--- src/main/java/com/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -3,10 +3,34 @@ package com.moodanalyser;
 public class MoodAnalyser {
 
     public String analyseMood(String message) {
-        if (message.contains("SAD")) {
+        if (message.contains("Sad")) {
             return "SAD";
         } else {
             return "HAPPY";
+        }
+    }
+
+    public String moodAnalyseMoodContainsNull(String message) {
+        try {
+            if (message.contains("Sad")) {
+                return "SAD";
+            } else {
+                return "HAPPY";
+            }
+        } catch (NullPointerException e) {
+            return "HAPPY";
+        }
+    }
+
+    public String moodAnalyzerException(String message) throws MoodAnalyserException {
+        try {
+            if (message.contains("Sad")) {
+                return "SAD";
+            } else {
+                return "HAPPY";
+            }
+        } catch (NullPointerException e) {
+            throw new MoodAnalyserException("Enter Valid Mood");
         }
     }
 }
--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -4,10 +4,32 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
+
+    MoodAnalyser moodAnalyser = new MoodAnalyser();
+
     @Test
-    public void testMoodAnalysis() throws Exception {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is a Happy message");
+    public void given_Message_When_Sad_Should_Return_Sad() {
+        MoodAnalyser moodAnalyser= new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a Sad Message");
+        Assertions.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void given_Message_When_Null_Should_Return_Happy() {
+        MoodAnalyser moodAnalyser= new MoodAnalyser();
+        String mood = moodAnalyser.moodAnalyseMoodContainsNull(null);
         Assertions.assertEquals("HAPPY", mood);
+    }
+
+    @Test
+    public void given_Message_When_Null_ReturnCustomException(){
+        try {
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            String mood = moodAnalyser.moodAnalyzerException(null);
+            Assertions.assertEquals("HAPPY", mood);
+        }catch (MoodAnalyserException e){
+            // Assertions.assertEquals("Enter Valid Mood",e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
